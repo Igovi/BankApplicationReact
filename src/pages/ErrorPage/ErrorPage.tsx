34fea816aaea512d8448eba6e28d704f81f6f877
Igovi <igovi9910@gmail.com>
2024-07-08 17:13:33 -0300

    Adicionando pagina de erro

--- src/pages/ErrorPage/ErrorPage.tsx
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -1,12 +1,17 @@
 import React, { FC } from 'react';
 import './ErrorPage.css';
+import Header from '../../components/Header';
 
 interface ErrorPageProps {}
 
 const ErrorPage: FC<ErrorPageProps> = () => (
-  <div className="ErrorPage">
-    ErrorPage Component
-  </div>
+  <div className="error-page-container">
+    <Header />
+      <div className="error-page">
+        <h1>Error 404</h1>
+        <p>page not found</p>
+      </div>
+    </div>
 );
 
 export default ErrorPage;
